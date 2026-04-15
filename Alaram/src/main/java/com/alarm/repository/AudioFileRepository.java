f4f0939b5f3c218520cbd2fd73f209db34b3d9f7
Anant Kumar Jain <anantkumarjain2006@gmail.com>
2026-04-15 19:18:08 +0530

    Delete Alaram directory

--- Alaram/src/main/java/com/alarm/repository/AudioFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alarm.repository;
-
-import com.alarm.model.AudioFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AudioFileRepository extends JpaRepository<AudioFile, Long> {
-}
